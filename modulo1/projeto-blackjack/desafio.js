fdf103fa22842a66057bdfabc3e504b9b59ac291
Lorran Christo dos Santos <lorransantos99@outlook.com>
2022-05-20 16:54:26 -0300

    Tentando fazer o desafio

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,3 +11,35 @@
  * 
  */
 
+// início
+const inicioDeJogo = confirm(
+  "Bem vindo ao Blackjack" + "\n" + "Deseja iniciar uma partida?"
+);
+
+// jogadores
+let usuario = [];
+let computador = [];
+
+if (inicioDeJogo) {
+  usuario.push(comprarCarta());
+  usuario.push(comprarCarta());
+  computador.push(comprarCarta());
+  computador.push(comprarCarta());
+  while (
+    (usuario[0].valor === 11 && usuario[1].valor === 11) ||
+    (computador[0].valor === 11 && computador[1].valor === 11)
+  ) {
+    usuario = [];
+    computador = [];
+    usuario.push(comprarCarta());
+    usuario.push(comprarCarta());
+    computador.push(comprarCarta());
+    computador.push(comprarCarta());
+  }
+}
+
+confirm(
+  `Suas cartas são ${usuario[0].texto} ${usuario[1].texto}` +
+    "\n" +
+    `A carta revelada do computador é ${computador[0].texto}`
+);
