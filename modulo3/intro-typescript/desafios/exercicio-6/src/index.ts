47d581d88c3963fc45ea5dc176a8cca593e0ce4b
Lorran Christo dos Santos <lorransantos99@outlook.com>
2022-08-10 18:15:01 -0300

    correção de variáveis

--- modulo3/intro-typescript/desafios/exercicio-6/src/index.ts
+++ modulo3/intro-typescript/desafios/exercicio-6/src/index.ts
@@ -2,20 +2,20 @@ const numbers = (): void => {
   const numberOne: number = Number(process.argv[2]);
   const numberTwo: number = Number(process.argv[3]);
 
-  let numeroMaior;
-  let numeroMenor;
+  let bigNumber;
+  let smallNumber;
 
   if (numberOne >= numberTwo) {
-    numeroMaior = numberOne;
-    numeroMenor = numberTwo;
+    bigNumber = numberOne;
+    smallNumber = numberTwo;
   } else {
-    numeroMaior = numberTwo;
-    numeroMenor = numberOne;
+    bigNumber = numberTwo;
+    smallNumber = numberOne;
   }
 
   const numberSum = numberOne + numberTwo;
-  const numberSub = numeroMaior - numeroMenor;
-  const numberDiv = numeroMaior / numeroMenor;
+  const numberSub = bigNumber - smallNumber;
+  const numberDiv = bigNumber / smallNumber;
   const numberMult = numberOne * numberTwo;
   console.log(
     `A soma dos números é ${numberSum} | A subtração dos números é ${numberSub} | A múltiplicação dos números é ${numberMult} | A divisão dos números é ${numberDiv}`
