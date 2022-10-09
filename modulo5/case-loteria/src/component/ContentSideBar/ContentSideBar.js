0621254fbad1f415c8bce1f9c7dcbbccf6136e02
lorransantos <lorransantos99@outlook.com>
2022-10-09 15:33:27 -0300

    finalizando-projeto

--- modulo5/case-loteria/src/component/ContentSideBar/ContentSideBar.js
+++ modulo5/case-loteria/src/component/ContentSideBar/ContentSideBar.js
@@ -5,10 +5,22 @@ import { BASE_URL } from '../../constants/baseUrl';
 import { useContext, useEffect, useState } from 'react';
 import SideBar from '../SideBar/SideBar';
 import { GlobalContext } from '../../global/GlobalContext';
-
+import { Sorteio } from './Sorteio';
 const ContentSideBar = () => {
   const { raffles, setRaffles, contest, setContest } =
     useContext(GlobalContext);
+  const [numeroSorteio, setNumeroSorteio] = useState({ concursoId: 0 });
+  const [numeros, setNumeros] = useState(0);
+  const [data, setData] = useState('10');
+
+  const colors = {
+    'MEGA-SENA': { cor: '#6BEFA3', index: 0 },
+    QUINA: { cor: '#8666EF', index: 1 },
+    LOTOFÁCIL: { cor: '#DD7AC6', index: 2 },
+    LOTOMANIA: { cor: '#FFAB64', index: 3 },
+    TIMEMANIA: { cor: '#5AAD7D', index: 4 },
+    'DIA DE SORTE': { cor: '#BFAF83', index: 5 },
+  };
 
   const getLottery = () => {
     axios
@@ -21,39 +33,76 @@ const ContentSideBar = () => {
 
   useEffect(getLottery, []);
 
+  const getLoteriasConcurso = () => {
+    axios
+      .get(
+        'https://brainn-api-loterias.herokuapp.com/api/v1/loterias-concursos',
+      )
+      .then((resposta) => {
+        setNumeroSorteio(resposta.data[colors[contest].index]);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
+
+  useEffect(getLoteriasConcurso, [contest]);
+
+  const getNumerosSorteados = () => {
+    axios
+      .get(
+        `https://brainn-api-loterias.herokuapp.com/api/v1/concursos/${numeroSorteio.concursoId}`,
+      )
+      .then((resposta) => {
+        setNumeros(resposta.data.numeros);
+        setData(modificarArray(resposta.data.data));
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
+  useEffect(getNumerosSorteados, [contest, numeroSorteio]);
+
+  const modificarArray = (data) => {
+    const resultado = data.split('-');
+    const mes = resultado[1];
+    const ano = resultado[0];
+    const day = resultado[2].toString().substring(0, 2);
+    const date = `${day}/${mes}/${ano}`;
+    return date;
+  };
+
   const getContest = () => {
     axios.get(`${BASE_URL}`);
   };
 
   return (
-    <Content>
-      {console.log(raffles.map((item) => item.nome))}
-      <Select
-        value={contest}
-        onChange={(event) => setContest(event.target.value)}
-      >
-        {raffles &&
-          raffles.map((item) => {
-            return (
-              <option
-                value={item.nome.toUpperCase()}
-                key={item.id}
-              >
-                {item.nome.toUpperCase()}
-              </option>
-            );
-          })}
-      </Select>
+    <>
+      <Content>
+        <Select
+          value={contest}
+          onChange={(event) => setContest(event.target.value)}
+        >
+          {raffles &&
+            raffles.map((item) => {
+              return (
+                <option value={item.nome.toUpperCase()} key={item.id}>
+                  {item.nome.toUpperCase()}
+                </option>
+              );
+            })}
+        </Select>
 
-      <h1>
-        <img
-          src={LogoSena}
-          alt=''
-        />
-        {contest}
-      </h1>
-      <h2>teste</h2>
-    </Content>
+        <h1>
+          <img src={LogoSena} alt="" />
+          {contest}
+        </h1>
+        <h2>
+          {numeroSorteio.concursoId} - {data}
+        </h2>
+      </Content>
+      <Sorteio numbers={numeros} />
+    </>
   );
 };
 
--- modulo5/case-loteria/src/component/ContentSideBar/style.js
+++ modulo5/case-loteria/src/component/ContentSideBar/style.js
@@ -23,4 +23,37 @@ export const Select = styled.select`
   padding: 8px;
   border-radius: 4px;
   background-color: #ffffff;
-`
+`;
+
+export const ContainerSorteio = styled.div`
+  top: 50%;
+  transform: translate(-50%);
+  right: 0px;
+  max-width: 800px;
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 40px;
+  gap: 20px;
+  justify-content: center;
+  color: black;
+  margin: 0px 40px;
+  @media (min-width: 912px) {
+    margin-right: 100px;
+  }
+  p {
+    font-weight: bold;
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    width: 90px;
+    height: 90px;
+    border-radius: 50px;
+    @media (min-width: 912px) {
+      width: 75px;
+      height: 75px;
+    }
+  }
+`;
